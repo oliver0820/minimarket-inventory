1fdeb6d03a10df2b7fc094548edd6e18b1e77f43
Luis Beato <luis.beato@ctc.local>
2025-04-03 19:59:54 -0400

    Agrega estructura completa con CRUD básico

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./routes/products');
 
+app.use(express.json());
 app.use(express.static('public'));
+app.use('/api/products', productRoutes);
 
-app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
+const PORT = 3000;
+app.listen(PORT, () => console.log('Servidor listo en http://localhost:'+PORT));
